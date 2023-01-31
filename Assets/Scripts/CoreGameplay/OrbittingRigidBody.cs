5200ea8db88a2c221dbf400d84588694c56eaeff
Spencer Rawls <rawlsspencer@gmail.com>
2023-01-30 18:38:01 -0800

    Fix death animation stick gravity for non-spherical gravity fields; Fix animation bugs; Add more character and information to character animations; Improve powerup card UI

--- Assets/Scripts/CoreGameplay/OrbittingRigidBody.cs
+++ Assets/Scripts/CoreGameplay/OrbittingRigidBody.cs
@@ -2,159 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(VariableDirection2DCharacterMover))]
 public class OrbittingRigidBody : MonoBehaviour
 {
-	private Rigidbody2D body;
+	[SerializeField] private float accelerationDueToGravity;
 
-	public float accelerationDueToGravity;
-	public bool keepRotation;
-
-	public delegate void CenterChangedDelegate();
-	public event CenterChangedDelegate OnOrbitCenterChanged;
-
-	public Transform orbitCenter
-	{
-		get; private set;
-	}
-
-	private Vector3 lastCenterPosition;
-
-	public float horizontalSpeed
-	{
-		get
-		{
-			Vector2 velocity = body.velocity;
-			if (orbitCenter == null)
-			{
-				return velocity.x;
-			}
-
-			Vector2 up = transform.position - orbitCenter.position;
-			up.Normalize();
-			Vector2 right = Vector3.Cross(up, Vector3.forward);
-			return Vector2.Dot(velocity, right);
-		}
-		set
-		{
-			Vector2 velocity = body.velocity;
-			if (orbitCenter == null)
-			{
-				velocity.x = value;
-				return;
-			}
-
-			Vector2 up = transform.position - orbitCenter.position;
-			up.Normalize();
-			Vector2 right = Vector3.Cross(up, Vector3.forward);
-
-			Vector2 upComp = up * Vector2.Dot(up, velocity);
-			Vector2 rightComp = right * value;
-			body.velocity = upComp + rightComp;
-		}
-	}
-
-	public float verticalSpeed
-	{
-		get
-		{
-			Vector2 velocity = body.velocity;
-			if (orbitCenter == null)
-			{
-				return velocity.x;
-			}
-
-			Vector2 up = transform.position - orbitCenter.position;
-			up.Normalize();
-			return Vector2.Dot(velocity, up);
-		}
-		set
-		{
-			Vector2 velocity = body.velocity;
-			if (orbitCenter == null)
-			{
-				velocity.x = value;
-				return;
-			}
-
-			Vector2 up = transform.position - orbitCenter.position;
-			up.Normalize();
-			Vector2 right = Vector3.Cross(up, Vector3.forward);
-
-			Vector2 upComp = up * value;
-			Vector2 rightComp = right * Vector2.Dot(right, velocity);
-			body.velocity = upComp + rightComp;
-		}
-	}
-
-	public float verticalPosition
-	{
-		get
-		{
-			Vector2 position = body.position;
-			if (orbitCenter == null)
-			{
-				return position.y;
-			}
-
-			return Vector2.Distance(position, orbitCenter.position);
-		}
-		set
-		{
-			Vector2 position = body.position;
-			if (orbitCenter == null)
-			{
-				position.y = value;
-			}
-			else
-			{
-				Vector3 up = transform.position - orbitCenter.position;
-				up.Normalize();
-				position = orbitCenter.position + up * value;
-			}
-			body.position = position;
-		}
-	}
-
-	public Vector2 down
-	{
-		get
-		{
-			return (orbitCenter != null) ?
-				(orbitCenter.position - transform.position).normalized :
-				Vector3.down;
-		}
-	}
+	private VariableDirection2DCharacterMover mover;
 
 	private void Awake()
 	{
-		body = GetComponent<Rigidbody2D>();
-		OnOrbitCenterChanged += () => lastCenterPosition = orbitCenter.position;
+		mover = GetComponent<VariableDirection2DCharacterMover>();
 	}
 
 	private void FixedUpdate()
 	{
-		verticalSpeed -= accelerationDueToGravity * Time.fixedDeltaTime;
-		Vector2 up = -down;
-		if (keepRotation)
-		{
-			transform.rotation = Quaternion.FromToRotation(Vector2.up, up);
-		}
-
-		//Vector3 movement = orbitCenter.position - lastCenterPosition;
-		//lastCenterPosition = orbitCenter.position;
-		//transform.position += movement;
-	}
-
-	private void OnTriggerEnter2D(Collider2D collider)
-	{
-		if (collider.gameObject.CompareTag("Planetoid"))
-		{
-			orbitCenter = collider.transform;
-			if (OnOrbitCenterChanged != null)
-			{
-				OnOrbitCenterChanged();
-			}
-		}
+		mover.velocity += accelerationDueToGravity * Time.deltaTime * Vector3.down;
+		//mover.Move(mover.down)
 	}
 }
